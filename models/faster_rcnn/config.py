e1fbc36ed838fd16a3d617949e970d85aab9ab41
eunseop <ddf159@naver.com>
2025-06-09 00:43:54 +0900

    del config.py cfg_res50_5fpn

--- models/faster_rcnn/config.py
+++ models/faster_rcnn/config.py
@@ -22,22 +22,3 @@ cfg_res50_4fpn = {
     'out_channel': 256
 }
 
-cfg_res50_5fpn = {
-    'name': 'Resnet50',
-    # 'min_sizes': [[16, 32], [64, 128], [256, 512], [512, 1024], [1024, 2048]],
-    # 'steps': [8, 16, 32, 64, 128],
-    # 'variance': [0.1, 0.2],
-    'clip': False,
-    # 'loc_weight': 1.0,
-    'gpu_train': True,
-    'batch_size': 4,
-    'ngpu': 1,
-    'epoch': 100,
-    'decay1': 70,
-    'decay2': 90,
-    'image_size': 840,
-    'pretrain': True,
-    'return_layers': {'layer1': 0, 'layer2': 1, 'layer3': 2, 'layer4': 3, 'layer5': 4},
-    'in_channel': 256,
-    'out_channel': 256
-}
